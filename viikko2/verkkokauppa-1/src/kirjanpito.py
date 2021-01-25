87342d978d99769ea614d3ab7378a98c7a4a91b8
Jukka Aho <ahojukka5@gmail.com>
2021-01-25 22:55:23 +0700

    Remove singleton code from Kirjanpito

--- viikko2/verkkokauppa-1/src/kirjanpito.py
+++ viikko2/verkkokauppa-1/src/kirjanpito.py
@@ -1,13 +1,4 @@
 class Kirjanpito:
-    __instanssi = None
-
-    @staticmethod
-    def get_instance():
-        if not Kirjanpito.__instanssi:
-            Kirjanpito.__instanssi = Kirjanpito()
-
-        return Kirjanpito.__instanssi
-
     def __init__(self):
         self.tapahtumat = []
 
