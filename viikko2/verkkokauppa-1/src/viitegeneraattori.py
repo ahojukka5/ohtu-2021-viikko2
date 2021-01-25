90319b16ce1d7bb37899a86fbe5972cfc5997d6a
Jukka Aho <ahojukka5@gmail.com>
2021-01-25 22:54:51 +0700

    Remove singleton code from Viitegeneraattori

--- viikko2/verkkokauppa-1/src/viitegeneraattori.py
+++ viikko2/verkkokauppa-1/src/viitegeneraattori.py
@@ -1,13 +1,4 @@
 class Viitegeneraattori:
-    __instanssi = None
-
-    @staticmethod
-    def get_instance():
-        if not Viitegeneraattori.__instanssi:
-            Viitegeneraattori.__instanssi = Viitegeneraattori()
-
-        return Viitegeneraattori.__instanssi
-
     def __init__(self):
         self._seuraava = 1
 
