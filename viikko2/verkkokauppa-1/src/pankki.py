969fc06e2c83e962fa1ee9c64230db3a60797b8b
Jukka Aho <ahojukka5@gmail.com>
2021-01-25 22:55:48 +0700

    Remove singleton code from Pankki

--- viikko2/verkkokauppa-1/src/pankki.py
+++ viikko2/verkkokauppa-1/src/pankki.py
@@ -1,18 +1,6 @@
-from kirjanpito import Kirjanpito
-
-
 class Pankki:
-    __instanssi = None
-
-    @staticmethod
-    def get_instance():
-        if not Pankki.__instanssi:
-            Pankki.__instanssi = Pankki()
-
-        return Pankki.__instanssi
-
-    def __init__(self):
-        self._kirjanpito = Kirjanpito.get_instance()
+    def __init__(self, kirjanpito):
+        self._kirjanpito = kirjanpito
 
     def tilisiirto(self, nimi, viitenumero, tililta, tilille, summa):
         self._kirjanpito.lisaa_tapahtuma(
